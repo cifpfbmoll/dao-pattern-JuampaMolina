01abdc68caf35854a55cedc4fe70b27feff9e522
Juampa Molina <jpruizmolina@gmail.com>
2021-06-01 22:20:10 +0200

    refactor: return responses

--- src/main/java/org/acme/rest/json/FruitActiveRecord.java
+++ src/main/java/org/acme/rest/json/FruitActiveRecord.java
@@ -1,21 +1,27 @@
 package org.acme.rest.json;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
-public class ActiveRecord {
+public class FruitActiveRecord {
 
     public List<Fruit> getFruits() {
         return Fruit.findAll().list();
+    }
+
+    public Optional<Fruit> getFruit(String name) {
+        return Fruit.find("name", name).firstResultOptional();
     }
 
     public void addFruit(Fruit fruit) {
         fruit.persist();
     }
 
-    public void deleteFruit(Fruit fruit) {
+    public Fruit deleteFruit(Fruit fruit) {
         fruit.delete();
+        return fruit;
     }
 }
--- src/main/java/org/acme/rest/json/FruitResource.java
+++ src/main/java/org/acme/rest/json/FruitResource.java
@@ -2,49 +2,63 @@ package org.acme.rest.json;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.MediaType;
 
-import java.util.List;
+import java.util.Optional;
 
 import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.validation.constraints.NotNull;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.core.MediaType;
 
 @Path("/fruits")
 public class FruitResource {
 
     @Inject
-    ActiveRecord ar;
+    FruitActiveRecord ar;
+
+    public FruitResource() {
+    }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<Fruit> list() {
-        return ar.getFruits();
+    public Response list() {
+        return Response.ok(ar.getFruits(), MediaType.APPLICATION_JSON).build();
     }
 
-    @Consumes(MediaType.APPLICATION_JSON)
+    @GET
+    @Path("{name}")
     @Produces(MediaType.APPLICATION_JSON)
-    @Transactional
+    public Response getFruit(@NotNull @PathParam("name") String name) {
+        Optional<Fruit> fruit = ar.getFruit(name);
+        return fruit.isPresent() ? Response.ok(fruit).build()
+                : Response.status(Status.NOT_FOUND).entity("The fruit " + name + " doesn't exist.").build();
+    }
+
     @POST
-    public List<Fruit> add(Fruit fruit) {
-        ar.addFruit(fruit);
-        return list();
-    }
-
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     @Transactional
+    public Response add(Fruit fruit) {
+        ar.addFruit(fruit);
+        return Response.ok(fruit).build();
+
+    }
+
     @DELETE
     @Path("{name}")
-    public List<Fruit> delete(@NotNull @PathParam("name") String name) {
-        Fruit currentFruit = Fruit.find("name", name).firstResult();
-        System.out.println(currentFruit);
-        ar.deleteFruit(currentFruit);
-        return list();
+    @Produces(MediaType.APPLICATION_JSON)
+    @Transactional
+    public Response delete(@NotNull @PathParam("name") String name) {
+        Optional<Fruit> fruit = ar.getFruit(name);
+
+        return fruit.isPresent() ? Response.ok(ar.deleteFruit(fruit.get())).build()
+                : Response.status(Status.NOT_FOUND).entity("The fruit " + name + " doesn't exist.").build();
     }
 }
